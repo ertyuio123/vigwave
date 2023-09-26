656f4cb36fdebab427c25fa8e5f61724d8db01cb
태헌 <gkdntm2004@naver.com>
2023-09-26 16:59:20 +0900

    bootstrap

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# imsi.html 처리
+def imsi(request):
+    return render(request,
+                  'mainapp/imsi.html',
+                  {})
+
 # index.html 처리
 def index(request):
     return render(request,
